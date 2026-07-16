f44c1f5e420e5d499123e30414a12bbfa01f1ef2
hamamhany <famtmabatata@gmail.com>
2026-07-16 19:57:35 +0300

    .

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe3-qMRUVjYeBOW-OBZUApNn9IpTecGGk",
-  authDomain: "readandrise-e1420.firebaseapp.com",
-  projectId: "readandrise-e1420",
-  storageBucket: "readandrise-e1420.firebasestorage.app",
-  messagingSenderId: "685156970755",
-  appId: "1:685156970755:web:04d9c685084b2df835f936",
-  measurementId: "G-4LKJCL8PV2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
