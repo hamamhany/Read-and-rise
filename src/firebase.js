aa6cb689e84d6302d3b4b94f47f8b671af728cca
hamamhany <famtmabatata@gmail.com>
2026-07-21 23:36:51 +0300

    f

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe3-qMRUVjYeBOW-OBZUApNn9IpTecGGk",
-  authDomain: "readandrise-e1420.firebaseapp.com",
-  projectId: "readandrise-e1420",
-  storageBucket: "readandrise-e1420.firebasestorage.app",
-  messagingSenderId: "685156970755",
-  appId: "1:685156970755:web:04d9c685084b2df835f936",
-  measurementId: "G-4LKJCL8PV2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const messaging = getMessaging(app);
