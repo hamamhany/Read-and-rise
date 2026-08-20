992c8ae7bb75af0670a411274d06b5246d727cf3
hamamhany <famtmabatata@gmail.com>
2026-08-20 09:29:01 +0300

    4

--- src/components/panels/TeacherPanel.jsx
+++ src/components/panels/TeacherPanel.jsx
@@ -24,7 +24,7 @@ import { CountdownTimer } from '../common/CountdownTimer';
 import { ChoiceModal } from '../common/ChoiceModal';
 import { AddAssignmentModal } from '../common/AddAssignmentModal';
 import { AddLessonModal } from '../common/AddLessonModal';
-import { ZoomMeetingModal } from '../common/ZoomMeetingModal';
+import { AgoraMeetingModal } from '../common/AgoraMeetingModal';
 import { 
   generateId, sanitizeInput, arabicToEnglishNumber, 
   fetchClassNames, cleanPhoneNumber 
